ac8b515ea280a9dc8eadd85a39ede676b68179b5
hunter-steele <breakpoint.book@gmail.com>
2025-07-20 18:38:16 -0400

    Create app-vault-snippet.java
    Chapter 10

---
+++ app-vault-snippet.java
@@ -0,0 +1,15 @@
+```java
+// Decompiled MainActivity.java from secret.apk (App Vault challenge)
+// Shows hardcoded flag
+
+package com.example.secret;
+
+public class MainActivity {
+    private String flag = "CTF{4pp_v4ult_br34k}";
+
+    public void onCreate() {
+        System.out.println("App started");
+        // Flag is unused in logic, left by careless developer
+    }
+}
+```
